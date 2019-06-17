33e6f8c566a97b9ee57ae9e7063815ba371b545c
EC2 Default User <ec2-user@ip-10-0-0-69.us-west-2.compute.internal>
2019-06-17 23:16:39 +0000

    removing aws credentials argument

--- build_run_ei_container.sh
+++ build_run_ei_container.sh
@@ -2,10 +2,4 @@
 
 docker build . -t aws-tf1.13-ei1.1-video-object-detection 
 
-AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
-
-docker run -it --rm \
-   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-   -p 8888:8888 aws-tf1.13-ei1.1-video-object-detection
+docker run -it --rm -p 8888:8888 aws-tf1.13-ei1.1-video-object-detection
